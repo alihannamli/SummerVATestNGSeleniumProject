e896b7064806a7aa7ef418c4c6d4a34ff3780e7c
Alihan Namli <namlialihan@gmail.com>
2019-12-13 22:11:38 -0500

    commit

--- src/test/java/com/cybertek/tests/d14_properties_driver_class_test_base/SingletonTest.java
+++ src/test/java/com/cybertek/tests/d14_properties_driver_class_test_base/SingletonTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.d14_properties_driver_class_test_base;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SingletonTest {
@@ -10,12 +13,35 @@ public class SingletonTest {
       //  Singleton s = new Singleton(); --> compile error.
 
         String s1 = Singleton.getInstance();
-
         String s2 = Singleton.getInstance();
 
         System.out.println("s1 = "+s1);
         System.out.println("s2 = "+s2);
 
+    }
+    @Test
+    public void test2(){
+        // this is how we create WebDriver object from now on
+        // the type of the driver will come from the properties file
+        WebDriver driver = Driver.get();
+
+        String url = ConfigurationReader.get("url");
+        driver.get(url);
+        // we use this method to close the driver
+        Driver.closeDriver();
+    }
+    @Test
+    public void test3(){
+        // this is how we create WebDriver object from now on
+        // the type of the driver will come from the properties file
+
+        String url = ConfigurationReader.get("url");
+        Driver.get().get(url);
+        System.out.println(Driver.get().getTitle());
+        // we use this method to close the driver
+        Driver.closeDriver();
+
+        // 1;31;0 de kaldim
     }
 
 }
